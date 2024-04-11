27d66c84e2a56bdf496a43850936a2d9a4a9614d
Leo Dong <>
2024-04-11 17:19:18 +0800

    add framework

---
+++ UnityClient/Assets/GameScripts/HotFix/GameLogic/Demo/Actor/EntityEffect.cs
@@ -0,0 +1,17 @@
+﻿using GameLogic;
+using TEngine;
+using UnityEngine;
+
+public class EntityEffect : MonoBehaviour
+{
+	public float DelayDestroyTime = 1f;
+
+	public void InitEntity()
+	{
+		Invoke(nameof(DelayDestroy), DelayDestroyTime);
+	}
+	private void DelayDestroy()
+	{
+		PoolManager.Instance.PushGameObject(this.gameObject);
+	}
+}
---
+++ UnityClient/Assets/GameScripts/HotFix/GameLogic/Demo/UI/UIGameOver.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TEngine;
+using UnityEngine.PlayerLoop;
+
+namespace GameLogic
+{
+    [Window(UILayer.UI)]
+    class UIGameOver : UIWindow
+    {
+        #region 脚本工具生成的代码
+        private GameObject m_goTop;
+        private Button m_btnStart;
+        private Button m_btnAbout;
+        private Text m_textVersion;
+        protected override void ScriptGenerator()
+        {
+            m_goTop = FindChild("m_goTop").gameObject;
+            m_btnStart = FindChildComponent<Button>("m_goTop/m_btnStart");
+            m_btnAbout = FindChildComponent<Button>("m_btnAbout");
+            m_textVersion = FindChildComponent<Text>("m_textVersion");
+            m_btnStart.onClick.AddListener(OnClickStartBtn);
+            m_btnAbout.onClick.AddListener(OnClickAboutBtn);
+        }
+        #endregion
+
+        #region 事件
+        private void OnClickStartBtn()
+        {
+            Log.Info("OnClickStartBtn");
+        }
+        private void OnClickAboutBtn()
+        {
+            Log.Info("OnClickAboutBtn");
+        }
+        #endregion
+
+        protected override void OnUpdate()
+        {
+            //Debug.Log("On update");
+        }
+    }
+}
